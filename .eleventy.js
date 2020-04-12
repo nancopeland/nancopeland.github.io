282bffe87cf64944534ec43be457910aa859eec1
Nan Copeland <ncopeland@industrydive.com>
2020-04-12 16:21:29 -0400

    changes output to docs folder

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,9 @@ module.exports = function(eleventyConfig) {
   	templateFormats: ["*"], // default
   });
   return {
-    passthroughFileCopy: true
+    passthroughFileCopy: true, 
+    dir: {
+      output: "docs"
+    }
   }
 }
